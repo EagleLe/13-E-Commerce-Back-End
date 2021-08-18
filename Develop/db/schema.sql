45a75cf7f6404e87563135cd34141fb8c7329559
eagle <eagle.lam.le@Gmail.com>
2021-08-18 17:54:41 -0400

    get Id

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,59 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
 
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `product_name` int(30) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT 10,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `producttag` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `tag_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+ALTER TABLE `producttag`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `producttag`
+  ADD CONSTRAINT `producttag_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `producttag_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
